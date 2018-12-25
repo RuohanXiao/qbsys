bb555d49f6fb9b7d297195a278b031e5a5d56113
RuohanXiao <402922811@qq.com>
2018-12-25 16:20:46 +0800

    content

--- src/views/components/custom_contentdiv.vue
+++ src/views/components/custom_contentdiv.vue
@@ -15,7 +15,7 @@
           </div>
         </Tooltip>
         <Tooltip placement="bottom" content="（Ctrl+A）" :delay="1000">
-          <div class="button-div">
+          <div class="button-div" @click='showAsList'>
             <Icon class="icon iconfont icon-list DVSL-bar-btn-new DVSL-bar-btn-back" size="26"></Icon>
             <p class="img-content">列表</p>
           </div>
@@ -68,26 +68,33 @@
       </div>
     </div>
     <div :style="{border:'1px solid rgba(54, 102, 116, 0.5)',margin:'0 10px',backgroundColor:'rgba(0,0,0,0.5)'}">
-      <Scroll :on-reach-bottom="handleReachBottom" v-if='!ifInfo' :height=ContentHeight>
-        <div id="contentchart" aria-autocomplete="true" :style="{height:ContentHeight,display:'flex',overflowY:'scroll'}">
-          <Row type="flex" justify="start" align="middle">
-            <Col :sm="8" :lg="6" align="middle" v-for="item in items">
-            <div class="contentDiv">
-              <p class="contentTitle" @click="showContent(item.id)">{{item.title}}</p>
-              <p class="contentText">{{item.text}}</p>
-              <p class="contentTime">{{item.time}}&nbsp;&nbsp;&nbsp;{{item.from}}</p>
-            </div>
-            </Col>
-            <Col span=24> <div @click="handleReachBottom">加载更多</div></Col>
-          </Row>
+      <div :style="{margin:'0,5px'}">
+      <div v-if="!showList">
+        <Scroll :on-reach-bottom="handleReachBottom" v-if='!ifInfo' :height=ContentHeight>
+          <div id="contentchart" aria-autocomplete="true" :style="{height:ContentHeight,display:'flex',overflowY:'scroll'}">
+            <Row type="flex" justify="start" align="middle">
+              <Col :sm="8" :lg="6" align="middle" v-for="item in items">
+              <div class="contentDiv" @click="showContent(item.id)">
+                <p class="contentTitle" >{{item.title}}</p>
+                <p class="contentText">{{item.text}}</p>
+                <p class="contentTime">{{item.time}}&nbsp;&nbsp;&nbsp;{{item.from}}</p>
+              </div>
+              </Col>
+              <Col span=24 v-if="items.length>0">
+              <div @click="handleReachBottom" :style="{textAlign:'center',color:'rgba(51,255,255,0.5)'}" class='more'>加载更多</div>
+              </Col>
+            </Row>
+          </div>
+        </Scroll>
+        <div id="contentInfo" v-if='ifInfo' :style="{height:ContentHeight,overflowY:'scroll'}">
+          <Icon class="icon iconfont icon-delete2 process-img DVSL-bar-btn-new DVSL-bar-btn-back" :style="{position:'absolute',right:'15px',top:'70px'}" size="26" @click='toContentDiv'></Icon>
+          <h2 class="contentInfoTitle" id='contentsTitle'></h2>
+          <p class="contentInfoTime" id='contentsTime'></p>
+          <p style='margin:30px'><span id='contents'></span></p>
         </div>
-        
-      </Scroll>
-      <div id="contentInfo" v-if='ifInfo' :style="{height:ContentHeight,overflowY:'scroll'}">
-        <Icon class="icon iconfont icon-delete2 process-img DVSL-bar-btn-new DVSL-bar-btn-back" :style="{position:'absolute',right:'15px',top:'70px'}" size="26" @click='toContentDiv'></Icon>
-        <h2 class="contentInfoTitle" id='contentsTitle'></h2>
-        <p class="contentInfoTime" id='contentsTime'></p>
-        <p style='margin:30px'><span id='contents'></span></p>
+      </div></div>
+      <div>
+      <div v-if="showList" :style="{height:ContentHeightList,overflowY:'scroll'}">这里列表</div>
       </div>
     </div>
     </Col>
@@ -110,6 +117,7 @@
       return {
         ifInfo: false,
         ContentHeight: 0,
+        ContentHeightList: 0,
         netheight: 0,
         netheightdiv: 0,
         netheightout: 0,
@@ -120,7 +128,8 @@
         page: 1,
         content: '',
         order: '',
-        toEdg: -100
+        toEdg: -100,
+        showList: false
       };
     },
     computed: mapState([
@@ -133,7 +142,12 @@
         // if(va[0].label.split('搜索:').length > 1) {
         mthis.content = va[0].label
         mthis.$http.get('http://10.60.1.140:5001/context-by-text/?page=1&query=' + mthis.content).then(response => {
-          mthis.items = response.body.data
+          if (response.body.data.length > 0) {
+            mthis.items = response.body.data
+            // mthis.showMore = true
+          } else {
+            // mthis.showMore = false
+          }
         })
         // }
         // }
@@ -145,6 +159,7 @@
       contentHeight: function() {
         var mthis = this;
         mthis.ContentHeight = mthis.$store.state.contentHeight - 75 + 'px';
+        mthis.ContentHeightList =  mthis.$store.state.contentHeight - 75 + 22 + 'px';
       }
     },
     components: {
@@ -152,6 +167,9 @@
     },
     props: ['contentData'],
     methods: {
+      showAsList() {
+        this.showList = true
+      },
       orderTimeUp() {
         var mthis = this
         mthis.order = '&isSortByTime=asc'
@@ -192,7 +210,7 @@
         });
       },
       handleReachBottom(status) {
-        alert()
+        // alert()
         var mthis = this
         mthis.page = mthis.page + 1
         return new Promise(resolve => {
@@ -206,13 +224,14 @@
         });
       },
       toContentDiv() {
+        this.showList = false
         this.ifInfo = false
         // document.getElementById('contents').innerHTML = ''
         // document.getElementById('contentsTitle').innerHTML = ''
         // document.getElementById('contentsTime').innerHTML = ''
       },
-       scrollBottom() {
-         ALERT('sss')
+      scrollBottom() {
+        //  ALERT('sss')
         // 滚动到页面底部时，请求前一天的文章内容
         if (((window.screen.height + document.body.scrollTop) > (document.body.clientHeight)) && this.REQUIRE) {
           // 请求的数据未加载完成时，滚动到底部不再请求前一天的数据
@@ -220,20 +239,20 @@
           this.loading = true;
           this.tips = '努力加载中...';
           alert('aaaa')
-        //   axios.get('http://zhihuapi.herokuapp.com/api/4/news/before/' + this.todayDate).then((res) => {
-        //     this.newsList.push(res.data['stories']);
-        //   this.date.push(res.data['date']);
-        //   this.todayDate = res.data['date'];
-        //   // 请求的数据加载完成后，再次滚动到底部时，允许请求前一天数据
-        //   this.$nextTick(() => {
-        //     this.REQUIRE = true;
-        //     this.loading = false;
-        //   });
-        // }).catch(() => {
-        //     this.tips = '连接失败，请稍后重试';
-        //   // 请求失败时，将 REQUIRE 置为 true，滚动到底部时，再次请求
-        //   this.REQUIRE = true;
-        // });
+          //   axios.get('http://zhihuapi.herokuapp.com/api/4/news/before/' + this.todayDate).then((res) => {
+          //     this.newsList.push(res.data['stories']);
+          //   this.date.push(res.data['date']);
+          //   this.todayDate = res.data['date'];
+          //   // 请求的数据加载完成后，再次滚动到底部时，允许请求前一天数据
+          //   this.$nextTick(() => {
+          //     this.REQUIRE = true;
+          //     this.loading = false;
+          //   });
+          // }).catch(() => {
+          //     this.tips = '连接失败，请稍后重试';
+          //   // 请求失败时，将 REQUIRE 置为 true，滚动到底部时，再次请求
+          //   this.REQUIRE = true;
+          // });
         }
       },
       showContent(id) {
@@ -315,12 +334,22 @@
   }
   .contentDiv {
     max-height: 255px;
-    padding: 5px;
+    padding: 5px 15px 5px 15px;
     overflow: hidden;
     background: rgba(51, 255, 255, 0.2);
     margin: 20px;
     background: linear-gradient(-135deg, transparent 10px, rgba(51, 255, 255, 0.2) 0);
     padding-top: 20px;
+    cursor: pointer;
+  }
+  .contentDiv:hover {
+    background: linear-gradient(-135deg, transparent 10px, rgba(51, 255, 255, 0.3) 0);
+    animation: all 1s;
+    -webkit-animation: all 1s;
+    /* transform:translate(5px,5px); */
+    -webkit-box-shadow:-5px 5px 10px -4px rgba(81, 85, 85, 0.5);
+    -moz-box-shadow: -5px 5px 10px -4px rgba(81, 85, 85, 0.5);
+    box-shadow: -5px 5px 10px -4px rgba(81, 85, 85, 0.5);
   }
   .contentDiv ::before {
     content: "";
@@ -329,18 +358,18 @@
     right: 20px;
     border-width: 0 16px 16px 0;
     border-style: solid;
-    border-color: rgba(0, 0, 0, 0) rgba(0, 0, 0, 0) rgba(51, 255, 255, 0.8) rgba(51, 255, 255, 0.8);
-    background: rgba(51, 255, 255, 0.8);
+    border-color: rgba(0, 0, 0, 0) rgba(0, 0, 0, 0) rgba(51, 255, 255, 0.5) rgba(51, 255, 255, 0.5);
+    background: rgba(51, 255, 255, 0.5);
     display: block;
     width: 0;
-    background: linear-gradient(-135deg, transparent 10px, rgba(51, 255, 255, 0.8) 0);
+    background: linear-gradient(-135deg, transparent 10px, rgba(51, 255, 255, 0.4) 0);
     -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3), -1px 1px 1px rgba(0, 0, 0, 0.2);
     -moz-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3), -1px 1px 1px rgba(0, 0, 0, 0.2);
     box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3), -1px 1px 1px rgba(0, 0, 0, 0.2);
   }
   .contentTitle {
     padding: 0 5px;
-    text-align: start;
+    text-align: center;
     color: #ccffff;
     cursor: pointer;
     font-family: MicrosoftYaHei;
@@ -442,5 +471,18 @@
     color: #ccffff;
     opacity: 0.8;
     line-height: 24px;
+  }
+  .more:hover {
+    cursor: pointer;
+    color: rgba(51, 255, 255, 0.8) !important;
+    transition: all 1s;
+    background: -webkit-linear-gradient( 8deg, rgba(0, 0, 0, 0) 0%, rgba(51, 255, 255, 0.2) 40%, rgba(51, 255, 255, 0.3) 50%, rgba(51, 255, 255, 0.2) 60%, rgba(0, 0, 0, 0) 100%);
+    /* Safari 5.1 - 6 */
+    background: -o-linear-gradient( 8deg, rgba(0, 0, 0, 0) 0%, rgba(51, 255, 255, 0.2) 40%, rgba(51, 255, 255, 0.3) 50%, rgba(51, 255, 255, 0.2) 60%, rgba(0, 0, 0, 0) 100%);
+    /* Opera 11.1 - 12*/
+    background: -moz-linear-gradient( 8deg, rgba(0, 0, 0, 0) 0%, rgba(51, 255, 255, 0.2) 40%, rgba(51, 255, 255, 0.3) 50%, rgba(51, 255, 255, 0.2) 60%, rgba(0, 0, 0, 0) 100%);
+    /* Firefox 3.6 - 15*/
+    background: linear-gradient( 8deg, rgba(0, 0, 0, 0) 0%, rgba(51, 255, 255, 0.2) 40%, rgba(51, 255, 255, 0.3) 50%, rgba(51, 255, 255, 0.2) 60%, rgba(0, 0, 0, 0) 100%);
+    /* 标准的语法 */
   }
 </style>
