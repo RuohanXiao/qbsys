af56ba4e2bc2460cbac8366632313cf2372dc76f
duchaofeng <duchaofeng@ict.ac.cn>
2019-06-05 16:01:37 +0800

    operHub

---
+++ src/views/components/operatorHub.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <Button @click="value2 = true" type="primary">Open</Button>
+    <Drawer title="Basic Drawer" placement="left" :closable="false" v-model="value2">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+    </Drawer>
+</div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+      }
+    },
+    components: {
+    }
+  }
+</script>
